43ab9dda4dc0641be7e5eedc1ca706c0218cfc74
bigduk88 <hangdeuk@naver.com>
2021-03-17 19:05:32 +0900

    21.3.17 알고리즘 풀이

--- week_2/algo5-1.py
+++ week_2/algo5-1.py
@@ -11,33 +11,31 @@
 첫째 줄에 DFS를 수행한 결과를, 그 다음 줄에는 BFS를 수행한 결과를 출력한다. V부터 방문된 점을 순서대로 출력하면 된다.
 '''
 
-def dfs(v):
-    print(v, end=' ')
-    visit[v] = 1
-    for i in range(1, n + 1):
-        if visit[i] == 0 and s[v][i] == 1:
-            dfs(i)
+N,M,V=map(int,input().split())                              #
+matrix=[[0]*(N+1) for i in range(N+1)]                      #
+for i in range(M):                                          #
+    a,b = map(int,input().split())                          #
+    matrix[a][b]=matrix[b][a]=1                             #
+visit_list=[0]*(N+1)                                        #
 
-def bfs(v):
-    queue = [v]
-    visit[v] = 0
-    while(queue):
-        v = queue[0]
-        print(v, end=' ')
-        del queue[0]
-        for i in range(1, n + 1):
-            if visit[i] == 1 and s[v][i] == 1:
-                queue.append(i)
-                visit[i] = 0
+def dfs(V):                                                 #
+    visit_list[V]=1                                         # 방문한 점 1로 표시                     
+    print(V, end=' ')                                       #
+    for i in range(1,N+1):                                  #
+        if(visit_list[i]==0 and matrix[V][i]==1):           #
+            dfs(i)                                          
 
-n, m, v = map(int, input().split())
-s = [[0] * (n + 1) for i in range(n + 1)]
-visit = [0 for i in range(n + 1)]
-for i in range(m):
-    x, y = map(int, input().split())
-    s[x][y] = 1
-    s[y][x] = 1
-    
-dfs(v)
-print()
-bfs(v)
+def bfs(V):                                                 #
+    queue=[V]                                               # 들려야 할 정점 저장
+    visit_list[V]=0                                         # 방문한 점 0으로 표시
+    while queue:                                            #
+        V=queue.pop(0)                                      #
+        print(V, end=' ')                                   #
+        for i in range(1, N+1):                             #
+            if(visit_list[i]==1 and matrix[V][i]==1):       #
+                queue.append(i)                             #
+                visit_list[i]=0                             #
+
+dfs(V)                                                      #
+print()                                                     #
+bfs(V)                                                      #
--- week_2/algo5-2.py
+++ week_2/algo5-2.py
@@ -21,3 +21,21 @@ N개의 수가 주어졌을 때, 네 가지 기본 통계값을 구하는 프로
 넷째 줄에는 범위를 출력한다.
 '''
 
+import sys
+from collections import Counter
+n = int(sys.stdin.readline())
+nums = []
+for i in range(n):
+    nums.append(int(sys.stdin.readline()))
+nums.sort()
+nums_s = Counter(nums).most_common()
+print(round(sum(nums) / n))
+print(nums[n // 2])
+if len(nums_s) > 1:
+    if nums_s[0][1] == nums_s[1][1]:
+        print(nums_s[1][0])
+    else:
+        print(nums_s[0][0])
+else:
+    print(nums_s[0][0])
+print(nums[-1] - nums[0])
